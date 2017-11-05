b6f52214c9c9390dbbc3f39a82b9735e5065fb55
Alejandro <fevsea@users.noreply.github.com>
2017-11-05 13:10:01 +0100

    Update urls.py

--- quedadas/urls.py
+++ quedadas/urls.py
@@ -5,12 +5,12 @@ from quedadas import views
 
 urlpatterns = [
     url(r'^$', views.api_root),
-    url(r'^users/login/', views.login),
-    url(r'^users/logout/', views.logout),
+    url(r'^users/login', views.login),
+    url(r'^users/logout', views.logout),
     url(r'^test/$', views.TestList.as_view(), name="test_list"),
     url(r'^meetings/$', views.MeetingList.as_view(), name="meeting_list"),
     url(r'^meetings/(?P<pk>[0-9]+)/$', views.MeetingDetail.as_view(), name="meeting_detail"),
-    url(r'^meetings/(?P<pk>[0-9]+)/participants/$', views.JoinMeeting.as_view(), name="join_meeting"),
+    url(r'^meetings/(?P<pk>[0-9]+)/participants$', views.JoinMeeting.as_view(), name="join_meeting"),
     url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
     url(r'^users/(?P<pk>[0-9]+)/meetings/$', views.UserMeeting.as_view(), name='user-meetings'),
