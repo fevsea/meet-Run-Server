a0265b525d57bfedfbdba03bbf0e1bd28ac8dd5b
Alejandro Agustin B <alejandro.a.es@ieee.org>
2017-11-27 08:22:15 +0100

    response create chat fixed

--- quedadas/views/chats.py
+++ quedadas/views/chats.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models import Q
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 
 from quedadas.models import Chat
 from quedadas.permissions import IsOwnerOrReadOnly
@@ -9,8 +10,7 @@ from quedadas.serializers import ChatSerializer, ChatSerializerCreate
 
 
 class ChatList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
-    serializer_class = ChatSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
         user = self.request.user
@@ -21,6 +21,15 @@ class ChatList(generics.ListCreateAPIView):
         if self.request.method in ('POST', 'PATCH', 'PUT'):
             return ChatSerializerCreate
         return ChatSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = ChatSerializerCreate(data=request.data)
+
+        if serializer.is_valid():
+            obj = serializer.save()
+            responseSerializer = ChatSerializer(obj)
+            return Response(responseSerializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ChatDetail(generics.RetrieveUpdateDestroyAPIView):
