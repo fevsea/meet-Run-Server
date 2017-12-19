b25595a68ccd403c109c1e4921e737141db529b0
Awais Iqbal Begum <awais.iqbal2@gmail.com>
2017-12-19 14:44:54 +0100

    added first 2 tests for meeting

--- populateDB.py
+++ populateDB.py
@@ -8,43 +8,45 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest.settings")
 django.setup()
 from quedadas.models import Meeting, Profile, Friendship
 from django.contrib.auth.models import User
-
-User.objects.create_superuser('admin', 'admin@example.com', 'meetnrun')
-
-userA = User.objects.create_user(username='alejandroA', password='alejandroA', first_name="Alejandro",
-                                 last_name="Agustin")
-Profile(user=userA, question="My question", answer="My answer", postal_code="08181", level=2).save()
-userB = User.objects.create_user(username='javierA', password='javierA', first_name="Javier", last_name="Alemán")
-Profile(user=userB, question="My question", answer="My answer", postal_code="08181", level=3).save()
-userC = User.objects.create_user(username='guillemC', password='guillemC', first_name="Guillem", last_name="Castro")
-Profile(user=userC, question="My question", answer="My answer", postal_code="08034", level=1).save()
-userD = User.objects.create_user(username='monicaF', password='monicaF', first_name="Mònica", last_name="Follana")
-Profile(user=userD, question="My question", answer="My answer", postal_code="08034", level=1).save()
-userE = User.objects.create_user(username='awaisI', password='awaisI', first_name="Awais", last_name="Iqbal")
-Profile(user=userE, question="My question", answer="My answer", postal_code="08034", level=1).save()
-userF = User.objects.create_user(username='marcP', password='marcP', first_name="Marc", last_name="Paricio")
-Profile(user=userF, question="My question", answer="My answer", postal_code="08034", level=2).save()
-userG = User.objects.create_user(username='ericR', password='ericR', first_name="Eric", last_name="Rodríguez")
-Profile(user=userG, question="My question", answer="My answer", postal_code="08034", level=1).save()
-
 from django.utils import timezone
 
-Friendship(creator=userA, friend=userB).save()
-Friendship(creator=userA, friend=userC).save()
-Friendship(creator=userC, friend=userA).save()
-Friendship(creator=userD, friend=userA).save()
-Friendship(creator=userE, friend=userF).save()
-Friendship(creator=userE, friend=userG).save()
+def populate():
+    User.objects.create_superuser('admin', 'admin@example.com', 'meetnrun')
 
-meetingA = Meeting(owner=userA, title="Sortida pel marenostrum", public=True, level=3, latitude="41.488576",
+    userA = User.objects.create_user(username='alejandroA', password='alejandroA', first_name="Alejandro",
+                                 last_name="Agustin")
+    Profile(user=userA, question="My question", answer="My answer", postal_code="08181", level=2).save()
+    userB = User.objects.create_user(username='javierA', password='javierA', first_name="Javier", last_name="Alemán")
+    Profile(user=userB, question="My question", answer="My answer", postal_code="08181", level=3).save()
+    userC = User.objects.create_user(username='guillemC', password='guillemC', first_name="Guillem", last_name="Castro")
+    Profile(user=userC, question="My question", answer="My answer", postal_code="08034", level=1).save()
+    userD = User.objects.create_user(username='monicaF', password='monicaF', first_name="Mònica", last_name="Follana")
+    Profile(user=userD, question="My question", answer="My answer", postal_code="08034", level=1).save()
+    userE = User.objects.create_user(username='awaisI', password='awaisI', first_name="Awais", last_name="Iqbal")
+    Profile(user=userE, question="My question", answer="My answer", postal_code="08034", level=1).save()
+    userF = User.objects.create_user(username='marcP', password='marcP', first_name="Marc", last_name="Paricio")
+    Profile(user=userF, question="My question", answer="My answer", postal_code="08034", level=2).save()
+    userG = User.objects.create_user(username='ericR', password='ericR', first_name="Eric", last_name="Rodríguez")
+    Profile(user=userG, question="My question", answer="My answer", postal_code="08034", level=1).save()
+
+
+
+    Friendship(creator=userA, friend=userB).save()
+    Friendship(creator=userA, friend=userC).save()
+    Friendship(creator=userC, friend=userA).save()
+    Friendship(creator=userD, friend=userA).save()
+    Friendship(creator=userE, friend=userF).save()
+    Friendship(creator=userE, friend=userG).save()
+
+    meetingA = Meeting(owner=userA, title="Sortida pel marenostrum", public=True, level=3, latitude="41.488576",
                    longitude="2.21284", date=(timezone.now() + timedelta(days=2)))
-meetingA.save()
-meetingA.participants.add(userB)
+    meetingA.save()
+    meetingA.participants.add(userB)
 
-meetingB = Meeting(owner=userA, title="Guanta", public=True, level=2, latitude="41.388576",
+    meetingB = Meeting(owner=userA, title="Guanta", public=True, level=2, latitude="41.388576",
                    longitude="2.11284", date=(timezone.now() + timedelta(days=3)))
-meetingA.save()
-meetingA.participants.add(userB, userC, userD, userE, userF)
+    meetingA.save()
+    meetingA.participants.add(userB, userC, userD, userE, userF)
 
-meetingC = Meeting(owner=userB, title="N'em a corre per l'eixample", public=False, level=1, latitude="41.378576",
+    meetingC = Meeting(owner=userB, title="N'em a corre per l'eixample", public=False, level=1, latitude="41.378576",
                    longitude="2.01284", date=(timezone.now() + timedelta(days=2))).save()
--- quedadas/tests/tests.py
+++ quedadas/tests/tests.py
@@ -3,8 +3,10 @@ import unittest
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-
+from django.contrib.auth.models import User
 from quedadas.models import Meeting
+from rest_framework.authtoken.models import Token
+from populateDB import populate
 
 
 class MeetingsTests(APITestCase):
@@ -26,3 +28,107 @@ class MeetingsTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Meeting.objects.count(), 1)
         self.assertEqual(Meeting.objects.get().title, 'Quedada1')
+
+class MeetingsTests(APITestCase):
+    def setUp(self):
+        populate()
+        self.user = User.objects.get(username='awaisI')
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        token.save()
+
+    def test_create_valid_meeting(self):
+        self.valid_payload = {
+            "title": "Testing Meeting",
+            "description": "bla bla bla",
+            "public": False,
+            "level": 1,
+            "date": "2017-11-28T10:52:39",
+            "latitude": "41.388576",
+            "longitude": "2.11284",
+            "chat": None
+        }
+        response = self.client.post(
+            reverse('meeting_list'),
+            data=self.valid_payload,
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        resp = {
+            'id': 3,
+            'title': 'Testing Meeting',
+            'description': 'bla bla bla',
+            'public': False,
+            'level': 1,
+            'date': '2017-11-28T10:52:39Z',
+            'latitude': '41.388576',
+            'longitude': '2.11284',
+            'owner': [{
+                'id': 6,
+                'username': 'awaisI',
+                'first_name': 'Awais',
+                'last_name': 'Iqbal',
+                'postal_code': '08034',
+                'question': 'My question',
+                'level': '1'
+            }],
+            'chat' : None
+        }
+        self.assertEqual(response.data, resp)
+
+    def test_create_invalid_notitle_meeting(self):
+        self.valid_payload = {
+            "description": "bla bla bla",
+            "public": False,
+            "level": 1,
+            "date": "2017-11-28T10:52:39",
+            "latitude": "41.388576",
+            "longitude": "2.11284",
+            "chat": None
+        }
+        response = self.client.post(
+            reverse('meeting_list'),
+            data=self.valid_payload,
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        resp = {
+            'title' : ['This field is required.']
+        }
+        self.assertEqual(response.data, resp)
+
+
+
+
+        expectedRespJson = {}
+        expectedRespJson['id'] = 3
+        expectedRespJson['title'] = 'Testing Meeting'
+        expectedRespJson['description'] = 'bla bla bla'
+        expectedRespJson['public'] = False
+        expectedRespJson['level'] = 1
+        expectedRespJson['date'] = '2017-11-28T10:52:39Z'
+        expectedRespJson['latitude'] = '41.388576'
+        expectedRespJson['longitude'] = '2.11284'
+        expectedRespJson['owner'] = repr({'id': 6,'username' : 'awaisI', 'first_name' : 'Awais', 'last_name' : 'Iqbal', 'postal_code': '08019', 'question' : 'My question', 'level' : '1'})
+        expectedRespJson['chat'] = None
+        expectedResp = {
+            'id': 3,
+            'title': 'Testing Meeting',
+            "description": "bla bla bla",
+            "public": False,
+            "level": 1,
+            "date": "2017-11-28T10:52:39",
+            "latitude": "41.388576",
+            "longitude": "2.11284",
+            'owner' : {
+                'id': 6,
+                'username': 'awaisI',
+                'first_name': 'Awais',
+                'last_name': 'Iqbal',
+                'postal_code': '08019',
+                'question': 'My question',
+                'level': '1'
+            },
+            expectedRespJson['chat'] : None
+        }
+
