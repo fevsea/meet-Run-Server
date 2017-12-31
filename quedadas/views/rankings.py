bf46ac2f883afba2c90d236b2f0a62f0508b279a
Alejandro Agustin <alejandro.a.es@ieee.org>
2017-12-31 15:28:10 +0100

    endpoint

--- quedadas/views/rankings.py
+++ quedadas/views/rankings.py
@@ -1,11 +1,23 @@
-from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, filters
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from quedadas.controllers import rankingsCtrl
 from quedadas.models import Zone
-from quedadas.serializers import ZoneSerializer
+from quedadas.serializers import ZoneSerializer, ZipSerializer
 
 
 class ZoneList(generics.ListAPIView):
     queryset = Zone.objects.order_by("-average")
     serializer_class = ZoneSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+    filter_fields = ('zip',)
 
+
+class ZipList(APIView):
+    def get(self, request):
+        zips = Zone.objects.only("zip")
+        serializer = ZipSerializer(zips, many=True)
+        return Response(serializer.data)
