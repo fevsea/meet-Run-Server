348292c63323ae5e14b5924aab865a8654755c37
Alejandro Agustin <alejandro.a.es@ieee.org>
2017-12-31 20:58:13 +0100

    thropy serializer and endpoint

--- quedadas/views/views.py
+++ quedadas/views/views.py
@@ -13,6 +13,7 @@ def api_root(request, format=None):
         'chats': reverse('chat-list', request=request, format=format),
         'challenges': reverse('challenge-list', request=request, format=format),
         'rankings': reverse('rankings-index', request=request, format=format),
+        'trophies': reverse('trophies', request=request, format=format),
         'admin': ''.join(['http://', get_current_site(request).domain, "/admin"]),
         'docs': reverse('docs', request=request, format=format),
     })
