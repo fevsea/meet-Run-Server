4379127d152ae0c7bed08ac3c4248eddda0d6e39
Alejandro Agustin B <alejandro.a.es@ieee.org>
2018-01-11 10:09:53 +0100

    baned endpoint and firebase

--- quedadas/controllers/userCtrl.py
+++ quedadas/controllers/userCtrl.py
@@ -1,5 +1,15 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
+from quedadas.controllers import firebaseCtrl
+
+
 def ban_request(user):
-    user.prof.ban_count += 1
-    if user.prof.ban_count >= 3:
-        user.prof.ban_count = 0
-        # Do ban
+    if user.prof.ban_date is None:
+        user.prof.ban_count += 1
+        if user.prof.ban_count >= 3:
+            user.prof.ban_count = 0
+            user.prof.ban_date = timezone.now() + timedelta(days=7)
+            user.prof.save()
+            firebaseCtrl.baned(user, 7)
